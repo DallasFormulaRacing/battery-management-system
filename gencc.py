55439b6d07dd13d0994f6a777fe1a2e4d51b4867
William Kim <73252368+wxkim@users.noreply.github.com>
2025-09-05 00:27:36 -0500

    Update gencc.py

--- gencc.py
+++ gencc.py
@@ -1,3 +1,7 @@
+# DISCLAIMER: THIS MAY NOT WORK OUT OF THE BOX ON YOUR DEVICE. 
+# EDIT AS NEEDED, BUT DO NOT FORGET TO GITIGNORE YOUR VERSION.
+# THIS IS JUST AN EXAMPLE (THAT HAPPENS TO WORK FOR ME)
+
 import os
 import json
 
@@ -9,9 +13,9 @@ include_dirs = [
     "-I./bms/Drivers/STM32G4xx_HAL_Driver/Inc",  
     "-I./bms/Drivers/CMSIS/Device/ST/STM32G4xx/Include",
     "-I./bms/Drivers/CMSIS/Include",
-    "-I/Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.3.1/include",
-    "-I/Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.3.1/include-fixed",
-    "-I/Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include"
+    "-I/Applications/ArmGNUToolchain/14.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.3.1/include",
+    "-I/Applications/ArmGNUToolchain/14.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.3.1/include-fixed",
+    "-I/Applications/ArmGNUToolchain/14.3.rel1/arm-none-eabi/arm-none-eabi/include"
 ]
 
 defines = [
