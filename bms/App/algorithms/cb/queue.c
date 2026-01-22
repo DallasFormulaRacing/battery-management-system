40af989c1c8038d97fc6458d717bf544aef38125
William Kim <will.kim0526@gmail.com>
2026-01-21 18:52:15 -0600

    add: queue files to cmake

--- bms/App/algorithms/cb/queue.c
+++ bms/App/algorithms/cb/queue.c
@@ -4,8 +4,15 @@ void init_queue(queue_t *cq) {
   cq->front = 0;
   cq->back = -1;
 }
+
+void reset_queue(queue_t *cq) {
+
+  cq->front = 0;
+  cq->back = -1;
+}
+
 bool is_empty(queue_t *cq) { return (cq->front == cq->back - 1); }
-bool is_full(queue_t *cq) { return NUM_CELL_MAX == cq->back; }
+bool is_full(queue_t *cq) { return (NUM_CELL_MAX == cq->back); }
 
 int16_t enq(queue_t *cq, battery_cell_t bc) {
   if (is_full(cq))
@@ -14,6 +21,7 @@ int16_t enq(queue_t *cq, battery_cell_t bc) {
   cq->pack[cq->back] = bc;
   return (int16_t)++cq->back;
 }
+
 int16_t deq(queue_t *cq) {
   if (is_empty(cq))
     return -1;
--- bms/App/algorithms/cb/queue.h
+++ bms/App/algorithms/cb/queue.h
@@ -1,12 +1,23 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include "cb.h"
+#include "bms_enums.h"
+#include "bms_types.h"
+#include "config.h"
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
-#define NUM_CELL_MAX 144
-#define NUM_PARALLEL_JOBS 8 // balance 8 cells at a time
+#define NUM_CELLS_PER_SEGMENT 12
+#define NUM_CELL_MAX (IC_COUNT_CHAIN * NUM_CELLS_PER_SEGMENT)
+#define NUM_PARALLEL_JOBS NUM_CELL_MAX
+
+typedef struct {
+  voltage_readings_t cell_voltage;
+  uint8_t cell_number;
+  uint8_t segment_number;
+  int16_t delta;
+} battery_cell_t;
 
 typedef struct {
   uint8_t num_parallel_balancing_allowed;
@@ -15,6 +26,7 @@ typedef struct {
 } queue_t;
 
 void init_queue(queue_t *cq);
+void reset_queue(queue_t *cq);
 bool is_empty(queue_t *cq);
 bool is_full(queue_t *cq);
 
