4c3f13a7952277aa5b1fad0657901f932ce07312
William Kim <will.kim0526@gmail.com>
2026-01-21 19:01:35 -0600

    add: fleshed out passive cell balancing struct

--- bms/App/algorithms/cb/cb.c
+++ bms/App/algorithms/cb/cb.c
@@ -1,13 +1,41 @@
 #include "cb.h"
+#include "bms_types.h"
 
 // ! table 95, page 68
 // ! needs to be refactored properly, just messy rn for testing purposes
 
 /**
+ * @brief
+ * Some things to think about:
+ * how do we want to collect the cells that need to be balanced?
+ * queue? all at once?
+ *
+ * collate all cell votlages from the asic array 192 element (asic) to 144
+ * element (pcb) -- is this necessary? -- not really but could make life easier
+ * we have a lot of memory to spare.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+/**
  * @brief cell balance super loop
  *
  *
- * @param asic_ctx: 
+ * @param asic_ctx:
  * @return void
  */
-void cell_delta_policy_enforcer(cell_asic_ctx_t *asic_ctx) {};
+void cell_delta_policy_enforcer(cell_asic_ctx_t *asic_ctx, pcb_ctx_t *pcb) {
+  // todo
+};
+
+void copy_cell_voltages(cell_asic_ctx_t *asic_ctx, pcb_ctx_t *pcb) {
+  // copy cells from asic
+}
--- bms/App/algorithms/cb/cb.h
+++ bms/App/algorithms/cb/cb.h
@@ -4,27 +4,48 @@
 #include "bms_enums.h"
 #include "bms_types.h"
 #include "config.h"
+#include "queue.h"
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
-  uint8_t lowest_cell_index;
-  voltage_readings_t lowest_cell_voltage;
-  //???
+  voltage_readings_t maximum_cell_delta_allowed;
+  battery_cell_t lowest_cell;
+  battery_cell_t batteries[NUM_CELL_MAX];
+  queue_t balance_q;
 } pcb_ctx_t;
 
-typedef struct {
-  voltage_readings_t cell_voltage;
-  uint8_t cell_number;
-  uint8_t segment_number;
-} battery_cell_t;
+void init_pcb(pcb_ctx_t *pcb);
 
 /**
  * use the retention registers to store segment identifier for easy id during
  * cell balancing
  *
  */
-
-void cell_delta_policy_enforcer(cell_asic_ctx_t *asic_ctx);
+void cell_delta_policy_enforcer(cell_asic_ctx_t *asic_ctx, pcb_ctx_t *pcb);
+void qsort_cells_by_voltage(battery_cell_t *cells, uint16_t num_cells);
 
 #endif
+
+// void sort_cell_queue(cell_asic_ctx_t *asic_ctx, pcb_ctx_t *pcb) {
+//   qsort_cells_by_voltage(pcb->balance_q.pack, NUM_PARALLEL_JOBS);
+// }
+
+// int comp(const void *CA, const void *CB) {
+//   const battery_cell_t *cellA = (const battery_cell_t *)CA;
+//   const battery_cell_t *cellB = (const battery_cell_t *)CB;
+
+//   if (cellA->cell_voltage > cellB->cell_voltage)
+//     return 1;
+
+//   if (cellA->cell_voltage < cellB->cell_voltage)
+//     return -1;
+
+//   return 0;
+// }
+
+// void qsort_cells_by_voltage(battery_cell_t *cells, uint16_t num_cells) {
+//   qsort(cells, num_cells, sizeof(battery_cell_t), comp);
+// }
