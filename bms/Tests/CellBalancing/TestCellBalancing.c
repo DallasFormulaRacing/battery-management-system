f02f61f08d5a400eadb9f2a941fdab10cce73c3b
alexshidagoatnocap <115957532+alexshidagoatnocap@users.noreply.github.com>
2026-01-29 22:10:45 -0600

    Tested map_delta_to_pwm_discretize

--- bms/Tests/CellBalancing/TestCellBalancing.c
+++ bms/Tests/CellBalancing/TestCellBalancing.c
@@ -1,6 +1,8 @@
 #include "../FFF/fff.h" // TODO: Fix this in CMake
+#include "bms_enums.h"
 #include "bms_types.h"
 #include "cb.h"
+#include "parse.h"
 #include "queue.h"
 #include "unity.h"
 #include "unity_internals.h"
@@ -74,6 +76,41 @@ void testCopyCellVoltages(void) {
   copy_cell_voltages(testAsicCtx, &testPcb);
 }
 
+static int16_t convert_human_readable_to_voltage(float voltage) {
+  return (int16_t)((voltage - 1.5F) / 0.000150F);
+}
+
+void testMapDeltaToPwmDiscretizeZero(void) {
+  testPcb.maximum_cell_delta_allowed = convert_human_readable_to_voltage(0.05F);
+  TEST_ASSERT_EQUAL_INT(PWM_0_0_PERCENT_DUTY_CYCLE,
+                        map_delta_to_pwm_discretize(
+                            &testPcb, convert_human_readable_to_voltage(0.0F)));
+}
+
+void testMapDeltaToPwmDiscretizeOne(void) {
+  testPcb.maximum_cell_delta_allowed = convert_human_readable_to_voltage(0.05F);
+  TEST_ASSERT_EQUAL_INT(
+      PWM_66_0_PERCENT_DUTY_CYCLE,
+      map_delta_to_pwm_discretize(
+          &testPcb, (int16_t)convert_human_readable_to_voltage(0.07F)));
+}
+
+void testMapDeltaToPwmDiscretizeMax(void) {
+  testPcb.maximum_cell_delta_allowed = convert_human_readable_to_voltage(0.01F);
+  TEST_ASSERT_EQUAL_INT(
+      PWM_100_0_PERCENT_DUTY_CYCLE,
+      map_delta_to_pwm_discretize(
+          &testPcb, (int16_t)convert_human_readable_to_voltage(0.1F)));
+}
+
+void testMapDeltaToPwmDiscretizeLowerThanMax(void) {
+  testPcb.maximum_cell_delta_allowed = convert_human_readable_to_voltage(0.05F);
+  TEST_ASSERT_EQUAL_INT(
+      PWM_0_0_PERCENT_DUTY_CYCLE,
+      map_delta_to_pwm_discretize(
+          &testPcb, (int16_t)convert_human_readable_to_voltage(0.03)));
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(testInitCellBalancingNegative);
@@ -82,5 +119,9 @@ int main(void) {
   RUN_TEST(testFindCellDeltasStart);
   RUN_TEST(testFindCellDeltasEnd);
   RUN_TEST(testFindCellDeltasMiddle);
+  RUN_TEST(testMapDeltaToPwmDiscretizeZero);
+  RUN_TEST(testMapDeltaToPwmDiscretizeOne);
+  RUN_TEST(testMapDeltaToPwmDiscretizeMax);
+  RUN_TEST(testMapDeltaToPwmDiscretizeLowerThanMax);
   return UNITY_END();
 }
