058353113c8ceaacdb745c5ad9b83c3d8d0f0006
alexshidagoatnocap <115957532+alexshidagoatnocap@users.noreply.github.com>
2026-02-04 15:12:34 -0600

    Changed LUT to reflect new LUT given by Aiden. I might just do a math formula to convert a human readable float to an array index because equalities on floats are sketch

--- bms/App/algorithms/thermal/thermistor.h
+++ bms/App/algorithms/thermal/thermistor.h
@@ -5,11 +5,10 @@
 #define NUM_THERMISTORS 1 // TODO: Change
 
 typedef struct {
-  temperature_readings_t temperature;
-  resistance_readings_t resistance;
-  // voltage_readings_t voltage;
+  voltage_human_readable_t voltage;
+  temp_c_readings_t temperature;
+  // resistance_readings_t resistance;
 } thermistor_t;
 
 resistance_readings_t get_resistance_from_voltage(voltage_readings_t voltage);
-temperature_readings_t get_temp_from_resistance(resistance_readings_t resistance);
-temperature_readings_t get_temp_from_voltage(voltage_readings_t voltage);
+temp_c_readings_t get_temp_from_voltage(voltage_human_readable_t voltage);
