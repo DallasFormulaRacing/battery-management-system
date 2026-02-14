87a650ef3baf6af03835b7475613834672d275ab
kushv818 <kushv818@gmail.com>
2026-02-14 14:43:24 -0600

    additional setup

---
+++ bms/App/core/source/can_gui_handler.c
@@ -0,0 +1,16 @@
+#include "can_utils.h"
+#include "can_gui_handler.h"
+//#include "fdcan.h"
+
+void can_readcavg_all(uint8_t* data){
+
+    return;
+}
+void can_readcv_all(uint8_t* data){
+    
+    return;
+}
+void can_readsv_all(uint8_t* data){
+    
+    return;
+}
--- bms/App/core/source/can_handler.c
+++ bms/App/core/source/can_handler.c
@@ -4,8 +4,6 @@
 //#include "node_tasks.h"
 //#include "cmsis_os2.h"
 
-
-
 void Process_CAN_Command(uint32_t cmd_id, uint8_t* data) {
     switch(cmd_id) {
         case CAN_CMD_ID_READCAVG_ALL:
