e9051bd63d07374f7a4d953b4fae81ca560e055b
William Kim <will.kim0526@gmail.com>
2026-03-02 13:20:36 -0600

    Revert "Merge branch 'master' into feat/sm/measurement_loop"
    This reverts commit 43f8e7d8d4e891d48f214cf11414cd2fac8025b8, reversing
    changes made to 7ea153de6fcc73ac5cab592f28be97b7ea6144ef.

--- bms/App/core/adc/adc.h
+++ bms/App/core/adc/adc.h
@@ -1,18 +1,1 @@
-#ifndef ADC_H
-#define ADC_H
 
-#include "stm32g4xx_hal.h"
-#include "stm32g4xx_hal_adc.h"
-
-#define C_VREF_ADC 3.3F
-#define C_ADC_RESOLUTION_12B 12
-
-extern ADC_HandleTypeDef hadc1;
-void adc_poll_no_DMA();
-void adc_start_no_DMA();
-void adc_stop_no_DMA();
-uint16_t adc_get_no_DMA();
-uint16_t adc_one_shot();
-float adc_convert_voltage(const uint16_t raw);
-
-#endif
