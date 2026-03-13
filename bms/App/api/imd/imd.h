8a31d3aaf94c4fa76fc8b6718270918abe0dec9b
William Kim <will.kim0526@gmail.com>
2026-03-12 22:09:46 -0500

    add: code gen for can1 and can2

---
+++ bms/App/api/imd/imd.h
@@ -0,0 +1,15 @@
+/**
+ * @file imd.h
+ * @author Will Kim
+ * @brief Isolation Monitoring Device
+ * @version 0.1
+ * @date 2026-03-12
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#ifndef IMD_H
+#define IMD_H
+
+#endif
---
+++ bms/App/lib/iso/inc/iso_parse.h
@@ -0,0 +1,9 @@
+#ifndef ISO_PARSE_H
+#define ISO_PARSE_H
+
+void imd_parse_general() {}
+void imd_parse_isolation() {}
+void imd_parse_voltage() {}
+void imd_parse_IT_system() {}
+
+#endif
