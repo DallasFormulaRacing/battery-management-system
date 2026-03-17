3864f0e182c15292d18fe51095fd69997c1376f6
kushv818 <kushv818@gmail.com>
2026-03-17 14:32:49 -0500

    helper functions for cleaner switch case

--- bms/App/lib/gui/src/gui_drivers.c
+++ bms/App/lib/gui/src/gui_drivers.c
@@ -12,6 +12,9 @@
 
 cell_asic_ctx_t *asic_array = hbms.asic;
 
+static void send_filtered_voltage_frame(int start_seg, int end_seg);
+static void send_therm_temp_frame(int start_seg, int end_seg);
+
 /*
  * HAL callback invoked when a new message arrives in RX FIFO0
  * (assuming notifications are enabled).
@@ -38,62 +41,25 @@ void process_can_command(uint32_t ext_id, uint8_t* data){
 
     switch(can_id_get_cmd(ext_id)){
         case CMD_ID_FIRST_24_CELLS:{
-            //build data function call with first 24 cell configured in parameters
-            uint8_t tx_frame[48];
-            cell_voltage_readings(asic_array, 0, 2, &tx_frame);
-
-            //send can frame with first_24_cells_resp as command id
-            can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
-            fdcan_send(tx_header, data, FDCAN_DLC_BYTES_48);
-            break;
+            send_filtered_voltage_frame(0, 2);
         }
         case CMD_ID_SECOND_24_CELLS:{
-            uint8_t tx_frame[48];
-            cell_voltage_readings(asic_array, 2, 4, &tx_frame);
-
-            //send can frame with first_24_cells_resp as command id
-            can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
-            fdcan_send(tx_header, data, FDCAN_DLC_BYTES_48);
-            break;
+            send_filtered_voltage_frame(2, 4);
         }
         case CMD_ID_THIRD_24_CELLS:{
-            uint8_t tx_frame[48];
-            cell_voltage_readings(asic_array, 4, 6, &tx_frame);
-
-            //send can frame with first_24_cells_resp as command id
-            can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
-            fdcan_send(tx_header, data, FDCAN_DLC_BYTES_48);
-            break;
+            send_filtered_voltage_frame(4, 6);
         }
 
         case CMD_ID_FOURTH_24_CELLS:{
-            uint8_t tx_frame[48];
-            cell_voltage_readings(asic_array, 6, 8, &tx_frame);
-
-            //send can frame with first_24_cells_resp as command id
-            can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
-            fdcan_send(tx_header, data, FDCAN_DLC_BYTES_48);
-            break;
+            send_filtered_voltage_frame(6, 8);
         }
 
         case CMD_ID_FIFTH_24_CELLS:{
-            uint8_t tx_frame[48];
-            cell_voltage_readings(asic_array, 8, 10, &tx_frame);
-
-            //send can frame with first_24_cells_resp as command id
-            can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
-            fdcan_send(tx_header, data, FDCAN_DLC_BYTES_48);
-            break;
+            send_filtered_voltage_frame(8, 10);
         }
 
         case CMD_ID_SIXTH_24_CELLS:{
-            uint8_t tx_frame[48];
-            cell_voltage_readings(asic_array, 10, 12, &tx_frame);
-
-            //send can frame with first_24_cells_resp as command id
-            can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
-            fdcan_send(tx_header, data, FDCAN_DLC_BYTES_48);
-            break;
+            send_filtered_voltage_frame(10, 12);
         }
 
         case CMD_ID_FIRST_60_TEMPS:
@@ -108,4 +74,26 @@ void process_can_command(uint32_t ext_id, uint8_t* data){
     }
     //switch case statement here to process the command and data received from the BMS
     return;
+}
+
+
+static void send_filtered_voltage_frame(int start_seg, int end_seg){
+    //build data function call with first 24 cell configured in parameters
+    uint8_t tx_frame[FDCAN_DLC_BYTES_48];
+    cell_voltage_readings(asic_array, start_seg, end_seg, &tx_frame);
+
+    //send can frame with first_24_cells_resp as command id
+    can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
+    fdcan_send(tx_header, tx_frame, FDCAN_DLC_BYTES_48);
+}
+
+
+static void send_therm_temp_frame(int start_seg, int end_seg){
+    //build data function call with first 24 cell configured in parameters
+    uint8_t tx_frame[FDCAN_DLC_BYTES_64];
+    cell_voltage_readings(asic_array, start_seg, end_seg, &tx_frame);
+
+    //send can frame with first_24_cells_resp as command id
+    can_ext_id_t tx_header = can_id_build(CAN_PRIORITY_P0, GUI_DEVICE_ID, CMD_ID_FIRST_24_CELLS, BMS_DEVICE_ID);
+    fdcan_send(tx_header, tx_frame, FDCAN_DLC_BYTES_64);
 }
