32c5406d449566ff948fb6bba7d5e79e441b9393
kushv818 <kushv818@gmail.com>
2026-03-25 02:30:48 -0500

    gui/can lib/core files transferred

---
+++ bms/App/lib/gui/inc/gui_drivers.h
@@ -0,0 +1,26 @@
+
+#ifndef GUI_DRIVER
+#define GUI_DRIVER
+
+#include "bms.h"
+#include "fdcan.h"
+#include "gui_types.h"
+
+#include "bms_types.h"
+#include "stm32g4xx_hal_fdcan.h"
+
+#include <stdint.h>
+
+void process_can_command(uint32_t ext_id, uint8_t *data);
+
+void send_can_error(can_error_id_t error_id);
+
+void cell_voltage_readings(cell_asic_ctx_t *asic, uint8_t start_ic, uint8_t end_ic,
+                           uint8_t *data_arr);
+
+void therm_temp_readings(cell_asic_ctx_t *asic, uint8_t start_ic, uint8_t end_ic,
+                         uint8_t *data_arr);
+
+void metadata_readings(pack_data_t *pack, pcb_ctx_t *pcb, uint8_t *data_arr);
+
+#endif
