103444190de8e75927647ca951fd81103cb87b7e
William Kim <will.kim0526@gmail.com>
2026-03-25 23:36:47 -0500

    fix: no static linkage error on can helper

--- bms/Core/Inc/main.h
+++ bms/Core/Inc/main.h
@@ -31,7 +31,6 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "safety_monitor.h"
 
 /* USER CODE END Includes */
 
