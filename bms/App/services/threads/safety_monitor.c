77fe7403edeae9ed940ac60a1efcf52c0cdfcc7a
William Kim <will.kim0526@gmail.com>
2026-03-25 23:21:27 -0500

    add: bms task 500ms tick instead of drift delay

--- bms/App/services/threads/safety_monitor.c
+++ bms/App/services/threads/safety_monitor.c
@@ -1,4 +1,6 @@
 #include "safety_monitor.h"
+#include "cmsis_os2.h"
+#include <stdint.h>
 
 const osThreadAttr_t bms_safety_task_attributes = {
     .name = "bms_safety_task",
@@ -7,8 +9,11 @@ const osThreadAttr_t bms_safety_task_attributes = {
 
 void bms_safety_task(void *argument) {
   bms_test_init();
+  const uint32_t task_freq = ((timing_parameters_t *)argument)->frequency;
+  uint32_t tick = osKernelGetTickCount();
   for (;;) {
     bms_test_run();
-    osDelay(1);
+    tick += task_freq;
+    osDelayUntil(tick);
   }
 }
