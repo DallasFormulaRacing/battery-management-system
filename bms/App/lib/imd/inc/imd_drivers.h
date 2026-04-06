df54557ceca8fe54da7ec3d88be97cc5b2945b88
Josh <josh@hessjd.com>
2026-04-06 16:19:48 -0500

    imd config commands

--- bms/App/lib/imd/inc/imd_drivers.h
+++ bms/App/lib/imd/inc/imd_drivers.h
@@ -6,7 +6,10 @@
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_hal_def.h"
 #include "stm32g4xx_hal_fdcan.h"
+#include <string.h>
 
 void imd_build_packet(IMD_CanId_t cmd, IMD_Packet_t *packet);
+void configure_imd_param();
+HAL_StatusTypeDef send_imd_cmd(IMD_Packet_t *packet);
 
 #endif
