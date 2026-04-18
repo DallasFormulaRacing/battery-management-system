fe85ff06742ccd2aeb0c9cff99c6cc053dee4211
William Kim <will.kim0526@gmail.com>
2026-04-18 15:41:16 -0500

    rn

--- bms/App/lib/elcon/inc/elcon_comms.h
+++ bms/App/lib/elcon/inc/elcon_comms.h
@@ -7,9 +7,9 @@
 #define ELCON_CHARGING_REQUEST_CAN_ID 0x1806E5F4
 #define ELCON_HEARTBEAT_CAN_ID 0x18FF50E5
 
-void elcon_send_command(elcon_command_t *command_profile);
+void elcon_send_command(elcon_internal_t *command_profile);
 
-can2_msg_t elcon_pack_can(elcon_command_t *command_profile);
+can2_msg_t elcon_pack_can(elcon_internal_t *command_profile);
 
 elcon_status_t elcon_handle_heartbeat(const can2_msg_t *msg);
 
