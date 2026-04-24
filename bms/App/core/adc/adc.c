1e74fb1c8f08a37d630319d3b6188c320851161e
William Kim <will.kim0526@gmail.com>
2026-04-24 17:47:33 -0500

    adc diff drivers

--- bms/App/core/adc/adc.c
+++ bms/App/core/adc/adc.c
@@ -1,28 +1,18 @@
 #include "adc.h"
 
-void adc_poll_no_DMA() {
-  (void)HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-}
-void adc_start_no_DMA() { //
-  (void)HAL_ADC_Start(&hadc1);
-}
-void adc_stop_no_DMA() {
-  //
-  (void)HAL_ADC_Stop(&hadc1);
-}
-
-uint16_t adc_one_shot() {
-  adc_start_no_DMA();
-  adc_poll_no_DMA();
-  return adc_get_no_DMA();
-}
-
-uint16_t adc_get_no_DMA() {
-  //
-  return HAL_ADC_GetValue(&hadc1);
-}
-
 float adc_convert_voltage(const uint16_t raw) {
   float scalar = ADC_REF_V * (float)raw;
   return scalar / ADC_RESOLUTION;
+}
+
+float adc_convert_to_diff_voltage(int16_t raw) {
+  return ((float)raw * ADC_REF_V) / ADC_RESOLUTION;
+}
+
+int16_t adc_one_shot_diff() {
+  HAL_ADC_Start(&hadc1);
+  if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
+    return (int16_t)HAL_ADC_GetValue(&hadc1);
+  }
+  return 0;
 }
--- bms/App/lib/amc/inc/amc_drivers.h
+++ bms/App/lib/amc/inc/amc_drivers.h
@@ -2,10 +2,12 @@
 #define AMC_DRIVERS_H
 
 #include "adc.h"
+#include <stdbool.h>
 #include <stdint.h>
 
 #define RESISTOR_DIV_RATIO 1001
+#define FAILSAFE_THRESHOLD -2.5F
 
-uint16_t get_hv_voltage();
+float get_hv_voltage(bool *is_fault);
 
 #endif
