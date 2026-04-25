6a5f09c4b8bd26b1aa10212bec8138349302c11d
William Kim <will.kim0526@gmail.com>
2026-04-25 00:20:07 -0500

    no prchrg ctrl

--- bms/App/services/threads/precharge/precharge.c
+++ bms/App/services/threads/precharge/precharge.c
@@ -2,16 +2,6 @@
 #include "amc_drivers.h"
 #include "cmsis_os2.h"
 
-static void close_precharge_relay(void) {
-  HAL_GPIO_WritePin(PRECHARGE_CIRCUIT_GPIO_GROUP, PRECHARGE_RELAY_GPIO,
-                    GPIO_PIN_SET);
-}
-
-static void open_precharge_relay(void) {
-  HAL_GPIO_WritePin(PRECHARGE_CIRCUIT_GPIO_GROUP, PRECHARGE_RELAY_GPIO,
-                    GPIO_PIN_RESET);
-}
-
 static void close_ir_pos_relay(void) {
   HAL_GPIO_WritePin(PRECHARGE_CIRCUIT_GPIO_GROUP, IR_POS_GPIO, GPIO_PIN_SET);
 }
@@ -22,12 +12,10 @@ static void open_ir_pos_relay(void) {
 
 void run_precharge_task() {
   // make sure both open
-  open_precharge_relay();
   open_ir_pos_relay();
   osDelay(50);
 
   // start precharging capacitor
-  close_precharge_relay();
 
   while (1) {
     osDelay(1);
@@ -38,5 +26,4 @@ void run_precharge_task() {
 
   close_ir_pos_relay();
   osDelay(50);
-  open_precharge_relay();
 }
