7979f5c18af8d7160bd7c6337a92429db4ab5202
William Kim <will.kim0526@gmail.com>
2026-04-24 22:43:59 -0500

    moving

--- bms/App/api/precharge/precharge.c
+++ bms/App/api/precharge/precharge.c
@@ -1,1 +1,30 @@
 #include "precharge.h"
+#include "cmsis_os2.h"
+
+static void close_precharge_relay(void) {
+  HAL_GPIO_WritePin(PRECHAGE_CIRCUIT_GPIO_GROUP, PRECHARGE_RELAY_GPIO,
+                    GPIO_PIN_SET);
+}
+
+static void open_precharge_relay(void) {
+  HAL_GPIO_WritePin(PRECHAGE_CIRCUIT_GPIO_GROUP, PRECHARGE_RELAY_GPIO,
+                    GPIO_PIN_RESET);
+}
+
+static void close_ir_pos_relay(void) {
+  HAL_GPIO_WritePin(PRECHAGE_CIRCUIT_GPIO_GROUP, IR_POS_GPIO, GPIO_PIN_SET);
+}
+
+static void open_ir_pos_relay(void) {
+  HAL_GPIO_WritePin(PRECHAGE_CIRCUIT_GPIO_GROUP, IR_POS_GPIO, GPIO_PIN_RESET);
+}
+
+void run_precharge_task() {
+  // make sure both open
+  open_precharge_relay();
+  open_ir_pos_relay();
+  osDelay(50);
+
+  // start prechargign capacitor
+  close_precharge_relay();
+}
