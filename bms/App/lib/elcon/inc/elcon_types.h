49c9cc18d34b7ba470375b0c9d32a72de85ff2d5
josh <josh@hessjd.com>
2026-06-02 18:06:02 -0500

    merge with master

--- bms/App/lib/elcon/inc/elcon_types.h
+++ bms/App/lib/elcon/inc/elcon_types.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef ELCON_TYPES_H
 #define ELCON_TYPES_H
 
@@ -21,4 +22,27 @@ typedef struct {
 
 extern elcon_internal_t hard_stop;
 
+=======
+#ifndef ELCON_TYPES_H
+#define ELCON_TYPES_H
+
+#include <stdint.h>
+
+typedef struct {
+  uint8_t hw : 1;
+  uint8_t temp : 1;
+  uint8_t input_voltage : 1;
+  uint8_t starting_state : 1;
+  uint8_t comm_state : 1;
+  uint8_t charger_OKAY : 1;
+} elcon_status_t;
+
+typedef struct {
+  uint16_t max_voltage;
+  uint16_t max_current;
+  uint8_t enable;
+  elcon_status_t heartbeat_msg;
+} elcon_command_t;
+
+>>>>>>> Stashed changes
 #endif
