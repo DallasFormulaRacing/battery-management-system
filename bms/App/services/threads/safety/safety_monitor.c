b965fdc19363050f5824fdb1cccdc62cca694be6
will kim <73252368+wxkim@users.noreply.github.com>
2026-07-12 17:35:04 -0500

    real shit
    Signed-off-by: will kim <73252368+wxkim@users.noreply.github.com>

--- bms/App/services/threads/safety/safety_monitor.c
+++ bms/App/services/threads/safety/safety_monitor.c
@@ -12,10 +12,12 @@ const timing_parameters_t bms_safety_task_time =
 
 void bms_safety_task(void *argument) {
   bms_test_init();
+  //bms_fsm_init(&hbms); // uncomment this for the real thing
   const uint32_t task_freq = ((timing_parameters_t *)argument)->frequency;
   uint32_t tick = osKernelGetTickCount();
   for (;;) {
     bms_test_run();
+    //bms_fsm_run(&hbms); // uncomment this for the real thing
     tick += task_freq;
     osDelayUntil(tick);
   }
